32f268352e02265cd4d5e1f3313e79f788bd5df4
Dante Falzone <claire0000@protonmail.com>
2021-09-20 14:39:01 -0500

    Created my first C# project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_dev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
