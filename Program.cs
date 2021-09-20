ac88def975c5acc144e81ea4f48862a8712f5580
Dante Falzone <claire0000@protonmail.com>
2021-09-20 15:33:46 -0500

    Wrote my first C# program

--- Program.cs
+++ Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 
-namespace csharp_dev
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine("Hello, world!");
     }
 }
